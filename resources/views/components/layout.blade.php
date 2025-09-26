ba8f284f9029e189727b257be398448018c46340
Daniel Villafranqui <danedo20472@gmail.com>
2025-09-25 23:41:45 -0500

    Chirp CRUD finished

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,7 +25,7 @@
 
     <!-- Success Toast -->
     @if (session('success'))
-        <div class="toast toast-top toast-center">
+        <div class="toast toast-top toast-center z-10">
             <div class="alert alert-success animate-fade-out">
                 <svg xmlns="<http://www.w3.org/2000/svg>" class="h-6 w-6 shrink-0 stroke-current" fill="none"
                     viewBox="0 0 24 24">
